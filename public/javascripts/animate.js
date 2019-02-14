02607bb480cf866924b73e2320c936222a7d1183
jensnti <jens.andreasson@ga.ntig.se>
2019-02-14 14:34:05 +0100

    final version

--- public/javascripts/animate.js
+++ public/javascripts/animate.js
@@ -1,35 +1,11 @@
-let l1, l2, l3, l1obj;
+let l1, l2, l3;
 
 l1 = document.getElementsByClassName('layer1')[0];
 l2 = document.getElementsByClassName('layer2')[0];
 l3 = document.getElementsByClassName('layer3')[0];
 
-l1obj = {
-    "delta": 1,
-    "x": 0,
-    "y": 0,
-    "step": 0
-}
-
-
-
-let i = 0;
-let delta = 1;
-let start;
-
-function step(timestamp) {
-    if (!start) start = timestamp;
-    var progress = timestamp - start;
-    l1obj.x = l1obj.x + (l1obj.step * l1obj.delta) / 100;
-    l1obj.y = l1obj.y + (l1obj.step * l1obj.delta) / 100;
-    l1.style = "transform: translate3d(" + l1obj.x + "px, " + l1obj.y + "px, 0px)";
-    l1obj.step++;
-
-    if (progress < 2000) {
-        l1obj.delta = 0;
-        l1obj.step = 0;
-        window.requestAnimationFrame(step);
-    }
-}
-  
-window.requestAnimationFrame(step);
+document.addEventListener("mousemove", (e) => {
+    l1.style = "transform: translate3d(" + e.x/100 + "px, " + e.y/50 + "px, 0px)";
+    l2.style = "transform: translate3d(" + e.x/500 + "px, " + e.y/100 + "px, 0px)";
+    l3.style = "transform: translate3d(" + e.x/1000 + "px, " + e.y/500 + "px, 0px)";
+});
