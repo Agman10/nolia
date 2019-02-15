b6dbeddca5355098b9895e6941f8da2311cec7e1
JensAndreasson <42906632+JensAndreasson@users.noreply.github.com>
2019-02-15 20:25:53 +0100

    centrerat, sass regler, flax från ntig.se

--- public/javascripts/animate.js
+++ public/javascripts/animate.js
@@ -1,11 +1,23 @@
-let l1, l2, l3;
+let l1, l2, l3, outX, outY;
 
-l1 = document.getElementsByClassName('layer1')[0];
-l2 = document.getElementsByClassName('layer2')[0];
-l3 = document.getElementsByClassName('layer3')[0];
+l1 = document.getElementsByClassName('parallax-container__layer1')[0];
+l2 = document.getElementsByClassName('parallax-container__layer2')[0];
+l3 = document.getElementsByClassName('parallax-container__layer3')[0];
 
-document.addEventListener("mousemove", (e) => {
-    l1.style = "transform: translate3d(" + e.x/100 + "px, " + e.y/50 + "px, 0px)";
-    l2.style = "transform: translate3d(" + e.x/500 + "px, " + e.y/100 + "px, 0px)";
-    l3.style = "transform: translate3d(" + e.x/1000 + "px, " + e.y/500 + "px, 0px)";
+function move(x, y, mod, el) {
+    el.style = "transform: translate3d(" + x / mod + "px, " + y / mod + "px, 0px)";   
+}
+
+document.addEventListener('mousemove', (e) => {
+    move(e.x, e.y, 20, l1);
+    move(e.x, e.y, 40, l2);
+    move(e.x, e.y, 200, l3);
+});
+
+document.addEventListener('mouseout', (e) => {
+    outX = e.x;
+    outY = e.y;
+});
+
+document.addEventListener('mouseenter', (e) => {
 });
